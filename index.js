f30348eff8299ae269ec3ea0cdbaa20692c79c87
asaad227 <a.saad227@yahoo.com>
2023-03-14 18:31:13 +0000

    update level to reload button bugs

--- index.js
+++ index.js
@@ -13,36 +13,62 @@ const btn12 = document.querySelector(".box12");
 const score = document.querySelector(".total");
 const level = document.querySelector(".round");
 const mainDiv = document.querySelector(".mainDiv");
+const mainDiv2 = document.querySelector(".mainDiv2");
 const divBox = document.querySelector(".divBox");
-const play = document.querySelector(".start");
+const play = document.querySelector(".play");
+const start = document.querySelector(".start")
 const remainingBox = document.querySelector(".remainingBox");
 const scoreDisplay = document.querySelector(".scoring");
 const abra = document.querySelector(".abra")
+const reload = document.querySelector(".reload");
+const endBtn = document.querySelector(".endBtn");
+const abra1 = document.querySelector(".abra1");
+const start1 = document.querySelector(".start1")
+// create box pick option for the player
 var pickBox = 7;
 let result= []
 var round = [];
 var count = []
-function start(){
-    const total = result.reduce((acc, curr)=> acc + curr, 0);
-    const game = round.length + 1
-    score.innerHTML = `Score: ${total}`;
-    level.innerHTML = `Level: ${game}`
-   mainDiv.className = "mainDiv1"
-   divBox.style.display = "initial";
-   play.style.display = "none";
-   abra.className = "abra1"
+// function start(){
+//     const total = result.reduce((acc, curr)=> acc + curr, 0);
+//     const game = round.length + 1
+//     score.innerHTML = `Score: ${total}`;
+//     level.innerHTML = `Level: ${game}`
+//     //change class name so change display according to page need
+//    mainDiv.className = "mainDiv1"
+//    divBox.style.display = "initial";
+//    play.style.display = "none";
+//    abra.className = "abra1"
    
-}
+// }
 
-play.addEventListener("click", start)
+// play.addEventListener("click", start)
 
 document.addEventListener("click", function(event){
-    count.push(1)
-    const remender = pickBox - count.length;
-    remainingBox.innerHTML = `Pick left: ${remender}`
+      if(event.target === play){
+        const total = result.reduce((acc, curr)=> acc + curr, 0);
+        const game = round.length + 1
+        score.innerHTML = `Score: ${total}`;
+        level.innerHTML = `Level: ${game}`;
+        //change class name so change display according to page need
+       mainDiv.className = "mainDiv1";
+       divBox.style.display = "initial";
+       abra.className = "abra1";
+       start.className = "start1";
+      }
+
+      if(event.target === reload){
+        console.log("i have clicked")
+       location.reload()
+    
+    }
+    //keep pick box counter on display 
+    let remender = pickBox - count.length;
+    remainingBox.innerHTML = `Pick left: ${remender }`
     const random = Math.floor(Math.random()*4);
     const randomBox = Math.floor(Math.random()*4)
     if(count.length === pickBox){
+       //nested codition to check round compeleted or not
         if(round.length > 0){
             level2()
         }else{
@@ -52,9 +78,15 @@ document.addEventListener("click", function(event){
         
     }else{
         if(event.target === btn1 ){
+            //if button click it will update counter and pickbox
+            count.push(1)
+            remender = pickBox - count.length;
+            remainingBox.innerHTML = `Pick left: ${remender }`
             if(random === randomBox){
+                // if this condition meet it will update result score and button class
                 result.push(10)
                 btn1.className = "boxG";
+                //disabled as soon as player this button, so no second pick
                 btn1.disabled = true;
             }else{
                 btn1.className = "boxP";
@@ -64,6 +96,9 @@ document.addEventListener("click", function(event){
         }
       
         if(event.target === btn2 ){
+            count.push(1)
+            remender = pickBox - count.length;
+            remainingBox.innerHTML = `Pick left: ${remender }`
             if(random === randomBox){
                 result.push(10)
                 btn2.className = "boxG";
@@ -74,6 +109,9 @@ document.addEventListener("click", function(event){
             }
         }
         if(event.target === btn3){
+            count.push(1)
+            remender = pickBox - count.length;
+            remainingBox.innerHTML = `Pick left: ${remender }`
             if(random === randomBox){
                 result.push(10)
                 btn3.className = "boxG";
@@ -85,6 +123,9 @@ document.addEventListener("click", function(event){
         }
       
         if(event.target === btn4){
+            count.push(1)
+            remender = pickBox - count.length;
+            remainingBox.innerHTML = `Pick left: ${remender }`
             if(random === randomBox){
                 result.push(10)
                 btn4.className = "boxG";
@@ -96,6 +137,9 @@ document.addEventListener("click", function(event){
         }
        
         if(event.target === btn5){
+            count.push(1)
+            remender = pickBox - count.length;
+            remainingBox.innerHTML = `Pick left: ${remender }`
             if(random === randomBox){
                 result.push(10)
                 btn5.className = "boxG";
@@ -107,6 +151,9 @@ document.addEventListener("click", function(event){
         }
        
         if(event.target === btn6){
+            count.push(1)
+            remender = pickBox - count.length;
+            remainingBox.innerHTML = `Pick left: ${remender }`
             if(random === randomBox){
                 result.push(10)
                 btn6.className = "boxG";
@@ -118,6 +165,9 @@ document.addEventListener("click", function(event){
         }
       
         if(event.target === btn7){
+            count.push(1)
+            remender = pickBox - count.length;
+            remainingBox.innerHTML = `Pick left: ${remender }`
             if(random === randomBox){
                 result.push(10)
                 btn7.className = "boxG";
@@ -129,6 +179,9 @@ document.addEventListener("click", function(event){
         }
         
         if(event.target === btn8 ){
+            count.push(1)
+            remender = pickBox - count.length;
+            remainingBox.innerHTML = `Pick left: ${remender }`
             if(random === randomBox){
                 result.push(10)
                 btn8.className = "boxG";
@@ -140,6 +193,9 @@ document.addEventListener("click", function(event){
         }
        
         if(event.target === btn9){
+            count.push(1)
+            remender = pickBox - count.length;
+            remainingBox.innerHTML = `Pick left: ${remender }`
             if(random === randomBox){
                 result.push(10)
                 btn9.className = "boxG";
@@ -151,6 +207,9 @@ document.addEventListener("click", function(event){
         }
 
         if(event.target === btn10){
+            count.push(1)
+            remender = pickBox - count.length;
+            remainingBox.innerHTML = `Pick left: ${remender }`
             if(random === randomBox){
                 result.push(10)
                 btn10.className = "boxG";
@@ -162,6 +221,9 @@ document.addEventListener("click", function(event){
         }
 
         if(event.target === btn11){
+            count.push(1)
+            remender = pickBox - count.length;
+            remainingBox.innerHTML = `Pick left: ${remender }`
             if(random === randomBox){
                 result.push(10)
                 btn11.className = "boxG";
@@ -173,6 +235,9 @@ document.addEventListener("click", function(event){
         }
 
         if(event.target === btn12){
+            count.push(1)
+            remender = pickBox - count.length;
+            remainingBox.innerHTML = `Pick left: ${remender }`
             if(random === randomBox){
                 result.push(10)
                 btn12.className = "boxG";
@@ -181,20 +246,23 @@ document.addEventListener("click", function(event){
                 btn12.className = "boxP";
                 btn12.disabled = true;
             }
-        }
-      
+        }  
     }
 
     const finalScore = result.reduce((acc, curr)=> acc + curr, 0);
     score.innerHTML = `Score: ${finalScore}`
 })
 
+
 function winner(){
+    // score set 
     const topScore = 10;
+    //keep update total score 
     var total = result.reduce((acc, curr)=> acc+curr, 0);
 
     if(total > topScore){
         alert(`Well done lets play next round!!`)
+        //run reset table for next level
         resetTable()
         document.querySelector(".boxes2").style.display = "block"
         pickBox = 10;
@@ -240,6 +308,7 @@ console.log(result)
 console.log(count)
 }
 function resetTable(){
+    // as all these boxes has been disbled so this function will enabled all the boxes
     btn1.className = "box1"
     btn1.disabled = false; 
     btn2.className = "box2"
@@ -264,18 +333,18 @@ function resetTable(){
     btn11.disabled = false;
     btn12.className = "box12"
     btn12.disabled = false;
+    // reset the counter for the round 
     count.length = 0; 
-
     
    }
 
    function level2(){
+    // set the score to beat
     const topScore = 30;
     const total = result.reduce((acc, curr)=> acc+curr, 0);
 
     if(total > topScore){
-        alert(document.querySelector(".result").innerHTML = "Congratulation you are the winner")
-      
+        document.querySelector(".result").innerHTML = "Congratulation you are the winner"
         level.style.display ="none"
         remainingBox.style.display = "none"
         scoreDisplay.className = "scoring1"
@@ -289,11 +358,10 @@ function resetTable(){
     }
 
     if(total < topScore){
-        alert(document.querySelector(".result").innerHTML = "Better luck next time")
+        document.querySelector(".result").innerHTML = "Better luck next time"
         level.style.display ="none"
         remainingBox.style.display = "none"
         scoreDisplay.className = "scoring1"
-    
         document.querySelector(".total").innerHTML = `Score: ${total}`
         document.querySelector(".endBtn").style.marginTop = "150px"
         document.querySelector(".heading").style.display = "none"
@@ -304,11 +372,10 @@ function resetTable(){
     }
 
     if(total === topScore){
-        alert(document.querySelector(".result").innerHTML = "Better luck next time")
+        document.querySelector(".result").innerHTML = "Better luck next time"
         level.style.display ="none"
         remainingBox.style.display = "none"
         scoreDisplay.className = "scoring1"
-        
         document.querySelector(".total").innerHTML = `Score: ${total}`
         document.querySelector(".endBtn").style.marginTop = "150px"
         document.querySelector(".heading").style.display = "none"
@@ -318,7 +385,8 @@ function resetTable(){
         document.querySelector("body").style.backgroundColor ="cadetblue"
     }
    
-  console.log("first")
+console.log("first")
 console.log(result)
 console.log(count)
 }
+
