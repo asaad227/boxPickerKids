347ad43a6ad485c7c5a85ffddb2d0e77f5052014
asaad227 <a.saad227@yahoo.com>
2023-03-17 18:43:46 +0000

    express server setup

---
+++ Server/db/connection.js
@@ -0,0 +1,27 @@
+import pkg from 'mongodb';
+const {MongoClient} = pkg;
+const connectionString = process.env.ATLAS_URI;
+const client = new MongoClient(connectionString,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+export async function connectToServer(callback) {
+  const result = await client.connect(function (err, db) {
+    if (err || !db) {
+      return callback(err);
+    }
+
+    dbConnection = db.db('Game');
+    console.log('Successfully connected to MongoDB.');
+
+    return callback();
+  });
+
+  return result;
+}
+export async function getDb() {
+  return await dbConnection;
+}
