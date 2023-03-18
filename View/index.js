1d2f2e244e3fc8601c41d7ef00bd0c21f197039b
asaad227 <a.saad227@yahoo.com>
2023-03-18 16:09:25 +0000

    leaderboard display loop fixed

--- View/index.js
+++ View/index.js
@@ -881,18 +881,35 @@ async function fetchPostUsers() {
     console.log(payload)
     var nameP = document.querySelector(".nameP");
     var scoreP = document.querySelector(".scoreP")
-    var nam; var total
-    for(let i = 0; i < 10; i++){
-        // sort need under the loop area
-        const sorted = Object.entries(payload).sort((a, b)=> b[1].score - a[1].score);
+    var nam; var total;
+    if(payload.length <=  1){
         var divN = document.createElement("div");
         var divS = document.createElement("div");
         nam = nameP.appendChild(divN);
         total = scoreP.appendChild(divS)
-            nam.innerHTML = `${sorted[i][1].name}`;
-            total.innerHTML = `${sorted[i][1].score}`;
-        
+        nam.innerHTML = payload[0].name;
+        total.innerHTML = payload[0].score
     }
+   
+if(payload.length > 1){
+    
+        const sorted = Object.entries(payload).sort((a, b)=> b[1].score - a[1].score);
+        console.log(sorted)
+        const nameMap = sorted.map(e=> e[1].name);
+        const scoreMap = sorted.map(e=> e[1].score);
+        for(let i = 0; i < nameMap.length; i++){
+            var divN = document.createElement("div");
+            var divS = document.createElement("div");
+            nam = nameP.appendChild(divN);
+            total = scoreP.appendChild(divS)
+                nam.innerHTML = `${nameMap[i]}`;
+                total.innerHTML =   `${scoreMap[i]}`;
+        }
+        
+        
+}
+    
+  
     }
     
  
